7272a8aef4b1d72678bab13203b638f2f2fcbf69
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-01-16 21:02:00 -0300

    update README and add the frontend React version boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container">
+      <header>
+        The Shop     
+      </header>
+      <small>Still under construction.</small>
+    </div>
+  );
+}
+
+export default App;
