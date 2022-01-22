28aac7c9a8ede69f6cbff85c52d9813119163938
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-01-21 23:35:44 -0300

    Login Page layout

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Header from '../components/Header'
 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 const Login = ({lang}) => {
     return (               
         <>
@@ -13,11 +17,46 @@ const Login = ({lang}) => {
                 headerUs="Login"
                 headerImage="user.png"                
             />
-            <div className="container">
-                <p>
-                    <Link to="/">Go to the home page</Link>
-                </p>
-            </div>
+            <section className="formLogin">
+                <div className="container">
+                    <Box
+                        className="formBox"
+                        component="form"
+                        sx={{
+                            width: 500,
+                            maxWidth: '100%',
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <div>
+                            <TextField
+                                //error
+                                id="email"
+                                label="E-mail"
+                                defaultValue=""
+                                fullWidth
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                //error
+                                id="password"
+                                label={lang == 'BR' ? 'Senha' : 'Password'}
+                                defaultValue="" 
+                                type="password"                             
+                                fullWidth
+                            />
+                        </div>
+                        <div>
+                            <Button variant="contained" className="button">Login</Button>
+                        </div>   
+                        <div>
+                            <Button variant="outlined" className="button">{lang == 'BR' ? 'Não tenho cadastro' : 'Register'}</Button>
+                        </div>                 
+                    </Box>
+                </div>
+            </section>            
         </>
     )
 }
