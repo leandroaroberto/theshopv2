825a46d8496cef10dd5513b61384c06d7c0a9471
Leandro Roberto <leandro.roberto@dextra-sw.com>
2022-01-27 23:16:12 -0300

    ContentDialog and index page layout update

--- frontend/src/components/ContentDialog.js
+++ frontend/src/components/ContentDialog.js
@@ -10,11 +10,13 @@ export default function ContentDialog({prodIdSelected, prods, handleClose, open,
   const selectedProd = prods.filter((prod) => prod.id == prodIdSelected)
   const [nome, setNome] = useState('')
   const [descricao, setDescricao] = useState('')
-  
-  useEffect(() => {      
+  const [fotoDestacada, setFotoDestacada] = useState('')
+
+  useEffect(() => { 
+    setFotoDestacada(selectedProd.length > 0 ? selectedProd[0].fotoDestacada : '')     
     if (lang == 'BR'){
       setNome(selectedProd.length > 0 ? selectedProd[0].nomeBr : '')
-      setDescricao(selectedProd.length > 0 ? selectedProd[0].caracteristicasBr : '')             
+      setDescricao(selectedProd.length > 0 ? selectedProd[0].caracteristicasBr : '')
     }
     else
     {      
@@ -31,17 +33,31 @@ export default function ContentDialog({prodIdSelected, prods, handleClose, open,
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        fullWidth={true}
+        maxWidth="md"
       >
         <DialogTitle id="alert-dialog-title">
           {nome}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {descricao}
-          </DialogContentText>
+          <div className="dialog">
+            <div>
+              <img src={`img/${prodIdSelected}/${fotoDestacada}`}/>
+            </div>
+            <div>
+              <p>
+                {descricao}
+              </p>
+              <p>
+                <strong>R$ {selectedProd.length > 0 ? selectedProd[0].preco : '0.00'}</strong>
+              </p>
+              <Button variant="contained" className="button">{lang == 'BR' ? 'Adicionar ao carrinho' : 'Add to cart'}</Button>              
+            </div>
+            
+          </div>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} autoFocus>
+          <Button onClick={handleClose} autoFocus color="secondary">
             {lang == 'BR' ? 'Fechar' : 'Close'}
           </Button>
         </DialogActions>
